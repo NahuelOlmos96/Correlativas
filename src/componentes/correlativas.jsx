4f46c012c06d339d3636e795fa5f3d9f990404d9
nahuel olmos <nahuelolmos22@gmail.com>
2022-08-23 15:32:02 -0300

    subiendo para resubir a una pagina

--- src/componentes/correlativas.jsx
+++ src/componentes/correlativas.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Materias } from './materias';
+import  {Materias}  from './materias';
     
 export const Correlativas = () =>{
 
-    
     
     return(
         <div>
             <h1>Correlativas</h1>
             <Materias />
-
+            
+            
             <footer> SRI</footer>
         </div>
     );
 } 
+
+
