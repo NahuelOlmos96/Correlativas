375492d4417dc0bec104700f55f61be2a782c6ae
Nahuel Olmos <nahuelolmos22@gmail.com>
2022-08-26 12:31:13 -0300

    colocando las materias en el app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-
+import React, {useState } from 'react';
 import './App.css';
-import { Correlativas } from './componentes/correlativas';
+import Pueba from './componentes/prueba';
+
+import materias  from './materias'
+
 
 function App() {
+
+  const[materia,setMaterias] = useState(materias)
+
+    
+  const valor1 = 1
+  const valor2 = 2
+        
+    console.log(materia)
+
+      function cambio(num, nombreCambiar){
+
+          const newArray = materia.map((comprovador)=>
+            {
+              if (comprovador.id === num){
+              comprovador.nombre = nombreCambiar;
+              
+            }
+            return comprovador
+          })
+          
+          console.log(newArray)
+          setMaterias(newArray)
+        
+      } 
+
+
+  
   return (
-    <div className="App">
-      <Correlativas />
+    <div className="App container">
+
+   
+      
+      <Pueba cambio={cambio}
+       valor1={valor1}
+       valor2={valor2}
+       materia={materia}/>
     </div>
   );
 }
 
 export default App;
+
